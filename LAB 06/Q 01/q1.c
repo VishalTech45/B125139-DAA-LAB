ff2a54f58bea57fc5eb53d22567bd5f9a7026b2a
Vishal Kumar <vishalworkplace89@gmail.com>
2026-09-06 17:03:05 +0530

    uploaded lab 06 assignment

---
+++ LAB 06/Q 01/q1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int n;
+    printf("Enter n:");
+    scanf("%d",&n);
+    int *fib= (int *)malloc(n * sizeof(int));
+    if(n<0){
+        printf("Invalid value of n");
+    }
+    fib[0]=0;
+    if(n>=1) fib[1]=1;
+    for(int i=2;i<=n;i++){
+        fib[i]=fib[i-1]+fib[i-2];
+    }
+    printf("The value of %dth fibonacci number is %d",n,fib[n]);
+    free(fib);
+    return 0;
+}
